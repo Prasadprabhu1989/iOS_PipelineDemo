6d0a087856d5cdd3abfe8e08d6cebe2c77183c7c
Prasad Prabhu <prasadprabhu1989@gmail.com>
2020-05-11 12:21:23 +0530

    tabbar

---
+++ PipelineDemo/PipelineDemo/CustomTabController.swift
@@ -0,0 +1,41 @@
+//
+//  CustomTabController.swift
+//  PipelineDemo
+//
+//  Created by Prasad Prabhu on 11/05/20.
+//  Copyright © 2020 Prasad Prabhu. All rights reserved.
+//
+
+import UIKit
+
+class CustomTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if #available(iOS 13, *) {
+          let appearance = UITabBarAppearance()
+        appearance.stackedLayoutAppearance.normal.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.green]
+        appearance.stackedLayoutAppearance.selected.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.red]
+        tabBar.standardAppearance = appearance
+        }
+        else{
+             UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.red], for: .selected)
+            UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.green], for: .normal)
+                   
+        }
+//
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
