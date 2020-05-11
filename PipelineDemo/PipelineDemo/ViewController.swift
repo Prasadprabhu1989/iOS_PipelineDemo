66ff2a1f069c00a1f021d61346bd3286dba90aa6
Prasad Prabhu <prasadprabhu1989@gmail.com>
2020-05-11 10:12:37 +0530

    dsd

--- PipelineDemo/PipelineDemo/ViewController.swift
+++ PipelineDemo/PipelineDemo/ViewController.swift
@@ -12,9 +12,67 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let t = RepeatingTimer(timeInterval: 3)
+        t.eventHandler = {
+            print("Timer Fired")
+        }
+        t.resume()
         // Do any additional setup after loading the view.
     }
 
 
 }
 
+class RepeatingTimer {
+
+    let timeInterval: TimeInterval
+    
+    init(timeInterval: TimeInterval) {
+        self.timeInterval = timeInterval
+    }
+    
+    private lazy var timer: DispatchSourceTimer = {
+        let t = DispatchSource.makeTimerSource()
+        t.schedule(deadline: .now() + self.timeInterval, repeating: self.timeInterval)
+        t.setEventHandler(handler: { [weak self] in
+            self?.eventHandler?()
+        })
+        return t
+    }()
+
+    var eventHandler: (() -> Void)?
+
+    private enum State {
+        case suspended
+        case resumed
+    }
+
+    private var state: State = .suspended
+
+    deinit {
+        timer.setEventHandler {}
+        timer.cancel()
+        /*
+         If the timer is suspended, calling cancel without resuming
+         triggers a crash. This is documented here https://forums.developer.apple.com/thread/15902
+         */
+        resume()
+        eventHandler = nil
+    }
+
+    func resume() {
+        if state == .resumed {
+            return
+        }
+        state = .resumed
+        timer.resume()
+    }
+
+    func suspend() {
+        if state == .suspended {
+            return
+        }
+        state = .suspended
+        timer.suspend()
+    }
+}
